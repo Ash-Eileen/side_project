fb79886d0a6732720ccf36c5f435b806abc6f925
Ashley Smith <ash.mash.134@gmail.com>
2020-07-13 15:30:27 +1000

    fixed heroku issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pets#index'
-  resources :pets
   get 'pets/random', to: 'pets#random', as: 'random'
+  resources :pets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
